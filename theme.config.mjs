8deec4e89916060415ea87aca05299f4f717e846
kevindsousa <kevindiegodasilvasousa@gmail.com>
2023-11-08 08:03:42 -0300

    feat: subindo projeto base

---
+++ theme.config.mjs
@@ -0,0 +1,13 @@
+export default {
+    logo: <span> Docs</span>,
+    project: {
+      link: "https://github.com/code-hike/codehike",
+    },
+    footer: {
+      text: 'Documentação linux personalizada',
+    },
+    feedback: {
+      content: () => <>Dúvidas?→</>,
+      labels: "feedback",
+    },
+  }
